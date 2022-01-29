ca53d794089289bd1e9f8e99f2c3c47cde9d2aa2
aschweitzer7 <aschweitzer7@gatech.edu>
2022-01-29 11:25:05 -0500

    Slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,50 @@
 			}, 100);
 		});
 
+	// Slideshow
+
+	
+	var currentSlide = 0;
+	const slides = document.querySelectorAll(".slide")
+	const dots = document.querySelectorAll('.dot')
+
+	const init = (n) => {
+	slides.forEach((slide) => {
+		slide.style.display = "none"
+		dots.forEach((dot) => {
+		dot.classList.remove("active")
+		})
+	})
+	slides[n].style.display = "block"
+	dots[n].classList.add("active")
+	}
+	document.addEventListener("DOMContentLoaded", init(currentSlide))
+
+	
+	const next = () => {
+		currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
+		init(currentSlide)
+	}
+	
+	const prev = () => {
+		currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
+		init(currentSlide)
+	}
+	
+	document.querySelector(".next").addEventListener('click', next)
+	document.querySelector(".prev").addEventListener('click', prev)
+
+	
+	dots.forEach((dot, index) => {
+		dot.addEventListener("click", () => {
+		init(index)
+		currentSlide = i
+		})
+	})
+
+	
+
+
 	// Nav.
 		var $nav = $('#nav');
 
@@ -69,6 +113,13 @@
 							$this
 								.addClass('active')
 								.addClass('active-locked');
+
+
+
+
+						
+
+
 
 					})
 					.each(function() {
@@ -119,5 +170,9 @@
 		$('.scrolly').scrolly({
 			speed: 1000
 		});
+
+
+
+		
 
 })(jQuery);
